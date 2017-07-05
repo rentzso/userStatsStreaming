b120a166670328590bafcad60cb9428761d2a872
Renzo Frigato <rfrigato@stanford.edu>
2017-07-04 17:14:59 -0700

    add scaladocs and comments

--- src/main/scala/insightproject/spark/userstatsstreaming/UserStatsStreaming.scala
+++ src/main/scala/insightproject/spark/userstatsstreaming/UserStatsStreaming.scala
@@ -21,20 +21,36 @@ import org.apache.spark.streaming.dstream.PairDStreamFunctions
 import scala.collection.JavaConversions._
 import java.io._
 
+/**
+  * Creates a Kafka Direct Stream to read user statistics into Spark Streaming and send them to Elasticsearch
+  */
 object UserStatsStreaming {
+  /**
+    * Creates the Avro schema used to create the Avro record
+    */
   val gdeltAvroSchema = {
     val parser = new Schema.Parser
     val schemaFile = getClass().getResourceAsStream("/avroSchemas/user-stats-avro-schema.json")
     parser.parse(schemaFile)
   }
-  val logFile = "/tmp/user-stats.log"
-  val bw = new BufferedWriter(new FileWriter(logFile))
 
+  /**
+    * Converts an Avro record into Bytes
+    */
   val recordInjection : Injection[GenericRecord, Array[Byte]] = GenericAvroCodecs.toBinary(gdeltAvroSchema)
+
+  /**
+    * Creates a Spark Streaming Context
+    *
+    * @param topic the topic we are reading from
+    * @param groupId the Consumer group id the context is using
+    * @param checkpointDirectory the folder were the checkpoint is stored
+    * @return
+    */
   def getContext(topic: String, groupId: String, checkpointDirectory: String): StreamingContext = {
     val bootstrapServers: String = sys.env.getOrElse("BOOTSTRAP_SERVERS", "localhost:9092")
     val kafkaParams = Map[String, Object](
-      "bootstrap.servers" -> bootstrapServers,
+      "bootstrap.servers" -> bootstrapServers, // Kafka bootstrap servers
       "key.deserializer" -> classOf[StringDeserializer],
       "value.deserializer" -> classOf[ByteArrayDeserializer],
       "group.id" -> groupId,
@@ -43,17 +59,18 @@ object UserStatsStreaming {
     )
     val sparkConf = new SparkConf().setAppName("user_stats")
     val esNodes: String = getEnvOrThrow("ES_NODES")
-    sparkConf.set("es.nodes", esNodes)
-    sparkConf.set("es.index.auto.create", "false")
-    sparkConf.set("es.batch.write.refresh", "false")
-    sparkConf.set("es.batch.size.entries", "1000")
-    sparkConf.set("es.net.http.auth.user", sys.env("ELASTIC_USER"))
-    sparkConf.set("es.net.http.auth.pass", sys.env("ELASTIC_PASS"))
-    sparkConf.set("spark.streaming.backpressure.enabled", "true")
+    sparkConf.set("es.nodes", esNodes) // the elasticsearch nodes
+    sparkConf.set("es.index.auto.create", "false") // do NOT create an index when it doesn't exist
+    sparkConf.set("es.batch.write.refresh", "false") // do NOT refresh on write
+    sparkConf.set("es.batch.size.entries", "1000") // size of the batch sent to elasticsearch
+    sparkConf.set("es.net.http.auth.user", sys.env("ELASTIC_USER")) // elasticsearch user
+    sparkConf.set("es.net.http.auth.pass", sys.env("ELASTIC_PASS")) // elasticsearch password
+    sparkConf.set("spark.streaming.backpressure.enabled", "true") // enable backpressure
     val timeWindow = sys.env.getOrElse("TIME_WINDOW", "5").toInt
     val ssc = new StreamingContext(sparkConf, Seconds(timeWindow))
     ssc.checkpoint(checkpointDirectory)
     val topics = Array(topic)
+    // Loads the location strategy from the ENV variable
     val locationStrategy = sys.env.get("LOCATION_STRATEGY") match {
       case Some("PreferBrokers") => PreferBrokers
       case _ => PreferConsistent
@@ -83,12 +100,24 @@ object UserStatsStreaming {
     ssc.start()
     ssc.awaitTermination()
   }
+  /**
+    * Gets the variable or throws an exception
+    *
+    * @param envVariable
+    * @return the value of the environment variable
+    */
   private def getEnvOrThrow(envVariable: String): String = {
     sys.env.get(envVariable) match {
       case Some(value) => value
       case None => throw new Exception(s"Environment variable $envVariable not set")
     }
   }
+  /**
+    * Parses a binary Avro message containing user statistics into a Map
+    *
+    * @param bytes
+    * @return the Map resulting from the Avro message
+    */
   private def convertAvroBytes(bytes:Array[Byte]) = {
     val record = recordInjection.invert(bytes).get
     Map(
